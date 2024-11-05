658a33f5c9ef10f24cd31e1f9666e1d20186d23f
Aztaaroth <jimmy.lundstein@gmail.com>
2024-11-05 10:46:04 +0100

    mini Kata 1

--- EdgoatKataWhite/MiniKata1/Program.cs
+++ EdgoatKataWhite/MiniKata1/Program.cs
@@ -1,2 +1,14 @@
-﻿
+﻿int playerHealth = 100;
+float playerAttack = 15.5f;
+bool isParalyzed = true;
+double playerCurrency = 24.75f;
 
+Console.WriteLine("Please enter your name");
+string name = Console.ReadLine();
+Console.Clear();
+
+Console.WriteLine($"{name}");
+Console.WriteLine($"Health: {playerHealth}");
+Console.WriteLine($"Attack Power: {playerAttack}");
+Console.WriteLine($"Paralyzed: {isParalyzed}"); 
+Console.WriteLine($"Gold Coins: {playerCurrency}");
